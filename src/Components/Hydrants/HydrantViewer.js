4436dc8a67769b7be4cd8c408e516b8d83765f43
unknown <khaled.kareem0506@gmail.com>
2019-10-05 16:23:23 -0400

    reducing iframe size, adding iframe css to keep it from exceeding container

--- src/Components/Hydrants/HydrantViewer.js
+++ src/Components/Hydrants/HydrantViewer.js
@@ -12,17 +12,17 @@ class HydrantViewer extends React.Component {
    render() {
     return (
         <div>
-            <Card>
+        <Card className="text-center">
                 <Card.Title> Old Johnny</Card.Title>
                 <Card.Text> Old Johnny is from San Juan, Puerto Rico </Card.Text>
                 <Card.Img variant="top" src={sample_hydrant} alt="sample hydrant" />
             </Card>
-            <br/>
-            <Card>
+            <Card className="text-center">
                 <Card.Title> Location </Card.Title>
                 <Card.Text> Old Johnny is from San Juan, Puerto Rico </Card.Text>
                 <Card.Body> 
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876754.5708617107!2d-68.74076090957921!3d18.398354668289443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c03686fe268196f%3A0xad6b7f0f5c935adc!2sSan%20Juan%2C%20Puerto%20Rico!5e0!3m2!1sen!2sca!4v1570304557657!5m2!1sen!2sca" width="400" height="300" frameborder="0" allowfullscreen=""></iframe>                </Card.Body>
+                    <iframe title="sample-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876754.5708617107!2d-68.74076090957921!3d18.398354668289443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c03686fe268196f%3A0xad6b7f0f5c935adc!2sSan%20Juan%2C%20Puerto%20Rico!5e0!3m2!1sen!2sca!4v1570304557657!5m2!1sen!2sca" width="400" height="300" frameborder="0" allowfullscreen=""></iframe> 
+                </Card.Body>
             </Card>
         </div>
         );
