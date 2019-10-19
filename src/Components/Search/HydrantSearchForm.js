41fc36ffe93dc35e8a9ffeeb2075b14a77ee3759
unknown <khaled.kareem0506@gmail.com>
2019-10-19 14:25:45 -0400

    getting broswer location, printing it. Need to add this value to the form

--- src/Components/Search/HydrantSearchForm.js
+++ src/Components/Search/HydrantSearchForm.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import {Button, FormControl, FormGroup, Form, FormLabel} from 'react-bootstrap';
+import {Button, FormControl, FormGroup, Form, FormLabel, Row, Col} from 'react-bootstrap';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import MapContainer from './MapContainer';
   
+const fetchUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log({lat: position.coords.latitude, long: position.coords.longitude});
+    });
+}
+
 class HydrantSearchForm extends React.Component {
    constructor() {
       super();
-      this.state = {
+        this.state = {
           searchName: "",
           searchRating: 'No filter',
+          lat:"",
+          long:"",
       }
     console.log('Entered Search form');
     this.updateName = this.updateName.bind(this);
+    this.setUserLocation = this.setUserLocation.bind(this);
     this.updateRating = this.updateRating.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
    }
@@ -22,6 +31,12 @@ class HydrantSearchForm extends React.Component {
     console.log(searchName, searchRating);
     this.props.onSubmit({ searchName, searchRating });
    }
+
+   setUserLocation(){
+       console.log(fetchUserLocation());
+
+    // this.setState({lat:position.coords.latitude, long: location.lat});
+}
    updateName(e) {
     console.log('updateName', e.target.value);
     this.setState({
@@ -37,12 +52,7 @@ class HydrantSearchForm extends React.Component {
 }
 
    render() {
-        const {searchName, searchRating} = this.state;
-
-        const mapStyles = {
-            width: '100%',
-            height: '100%'
-          };
+        const {searchName, searchRating, lat, long} = this.state;
           
         return (
         <Form onSubmit={this.onSubmit}>
@@ -73,11 +83,35 @@ class HydrantSearchForm extends React.Component {
                 </FormControl>
             </FormGroup>
             <FormGroup controlId="hydrantSearchRating">
-                {/* <FormControl> */}
-                    <FormLabel>Filter by Location</FormLabel>
-                    <MapContainer
-                    />
-                {/* </FormControl> */}
+                <FormLabel>Filter by Location</FormLabel>
+                <Row>
+                <Col>
+                <Button 
+                    onClick={this.setUserLocation}
+                > Get Your Location
+                </Button>
+                </Col>
+                <Col>
+                <FormControl 
+                    type="text"
+                    placeholder="Enter Latitude..."
+                    onChange={this.updateName}
+                    defaultValue={lat}
+                />
+                </Col>
+                <Col>
+                    <FormControl 
+                    type="text"
+                    placeholder="Enter Longitude..."
+                    onChange={this.updateName}
+                    defaultValue={long}
+                />
+                </Col>
+                </Row>
+
+                
+                {/* <MapContainer
+                /> */}
 
             </FormGroup>
 
