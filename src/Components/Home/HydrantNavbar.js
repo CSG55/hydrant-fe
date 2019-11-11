eafbba1f8e8bb2963e3da0135cd5d51aaf282db4
unknown <khaled.kareem0506@gmail.com>
2019-11-10 19:29:42 -0500

    fixing issue where navlink wouldnt close on click

--- src/Components/Home/HydrantNavbar.js
+++ src/Components/Home/HydrantNavbar.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter, Link} from 'react-router-dom';
 
 class HydrantNavbar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={ navExpanded: false };
+
+        this.setNavExpanded = this.setNavExpanded.bind(this);
+        this.onNavlinkClick = this.onNavlinkClick.bind(this);
+    }
+
+    setNavExpanded(expanded) {
+        this.setState({ navExpanded: expanded });
+    };
+    
+    onNavlinkClick(url) {
+        this.props.history.push(url);
+        this.setState({ navExpanded: false });
+    };
+    
       render() {
         return (
-            <Navbar className = "App-header" expand="lg" >
+            <Navbar onToggle={this.setNavExpanded} expanded={this.state.navExpanded} className = "App-header" expand="lg" >
                 <Navbar.Brand>
                     <Link to="/">Hydrant</Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link onClick={() => this.props.history.push('/')}>Home</Nav.Link>
-                    <Nav.Link onClick={() => this.props.history.push('register')}>Register</Nav.Link>
-                    <Nav.Link onClick={() => this.props.history.push('search')}>Search</Nav.Link>
-                    <Nav.Link onClick={() => this.props.history.push('create')}>Create</Nav.Link>
+                    <Nav.Link onClick={() => this.onNavlinkClick('/')}>Home</Nav.Link>
+                    <Nav.Link onClick={() => this.onNavlinkClick('register')}>Register</Nav.Link>
+                    <Nav.Link onClick={() => this.onNavlinkClick('search')}>Search</Nav.Link>
+                    <Nav.Link onClick={() => this.onNavlinkClick('create')}>Create</Nav.Link>
                     <NavDropdown title="Sample Pages" id="basic-nav-dropdown">
                     <NavDropdown.Item onClick={() => this.props.history.push('hydrant/1')}>Sample Hydrant</NavDropdown.Item>
                     </NavDropdown>
